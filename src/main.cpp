632a385f1e981cc9ce55eee3486e189facfb5ae1
Erick Martinez <erickmartinez0131@gmail.com>
2025-11-25 14:06:11 -0600

    added ray.h class

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "vec3.h"
+#include "color.h"
 
 int main() {
     int image_width = 256;
@@ -11,17 +13,12 @@ int main() {
         std::clog << "\rScanlines remaining: " << (image_height - i) << ' '
                   << std::flush;
         for (int j = 0; j < image_width; j++) {
-            auto r = double(j) / (image_width - 1);
-            auto g = double(i) / (image_height - 1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << "\n";
+            auto pixel_color = color(double(j) / (image_width - 1),
+                                     double(i) / (image_height - 1), 0);
+            write_color(std::cout, pixel_color);
         }
     }
+    std::clog << "\nDone.\n";
 
     return 0;
 }
--- src/vec3.h
+++ src/vec3.h
@@ -1,4 +1,4 @@
-#ifdef VEC3_H
+#ifndef VEC3_H
 #define VEC3_H
 
 #include <cmath>
@@ -6,6 +6,8 @@
 
 class vec3 {
    public:
+    double e[3];
+
     vec3() : e{0, 0, 0} {}
     vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
@@ -13,7 +15,7 @@ class vec3 {
     double y() const { return e[1]; }
     double z() const { return e[2]; }
 
-    vec3 operator~() const { return vec3(-e[0], -e[1], -e[2]); }
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
     double operator[](int i) const { return e[i]; }
     double &operator[](int i) { return e[i]; }
 
@@ -24,10 +26,10 @@ class vec3 {
 
         return *this;
     }
-    vec3 &operator*=(const vec3 &v) {
-        e[0] *= v.e[0];
-        e[1] *= v.e[1];
-        e[2] *= v.e[2];
+    vec3 &operator*=(double t) {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
 
         return *this;
     }
@@ -39,11 +41,40 @@ class vec3 {
     double length_squared() const {
         return (e[0] * e[0]) + (e[1] * e[1]) + (e[2] * e[2]);
     }
-
-   protected:
-    double e[3];
 };
 
-// Vector Utility Functions
+using point3 = vec3;
 
+// Vector Utility Functions
+std::ostream &operator<<(std::ostream &out, const vec3 &v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+vec3 operator+(const vec3 &u, const vec3 &v) {
+    return vec3((u.e[0] + v.e[0]), (u.e[1] + v.e[1]), (u.e[2] + v.e[2]));
+}
+
+vec3 operator-(const vec3 &u, const vec3 &v) {
+    return vec3((u.e[0] - v.e[0]), (u.e[1] - v.e[1]), (u.e[2] - v.e[2]));
+}
+
+vec3 operator*(double t, const vec3 &v) {
+    return vec3((t * v.e[0]), (t * v.e[1]), (t * v.e[2]));
+}
+
+vec3 operator*(const vec3 &v, double t) {
+    return t * v;
+}
+
+vec3 operator/(const vec3 &v, double t) {
+    return (1 / t) * v;
+}
+
+double dot(const vec3 &u, const vec3 &v) {
+    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
+}
+
+vec3 unit_vector(const vec3 &v) {
+    return v / v.length();
+}
 #endif
