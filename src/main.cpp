ad0ede4b80755bef0feed45995c2150f7ed218ee
Erick Martinez <erickmartinez0131@gmail.com>
2025-11-26 12:08:32 -0600

    made gradient image with rays

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,60 @@
 #include <iostream>
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
+
+color ray_color(const ray &r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
 
 int main() {
-    int image_width = 256;
-    int image_height = 256;
+    // Image
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+
+    // Calculate the image height, and ensure its >= 1
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width =
+        viewport_height * (double(image_width) / image_height);
+    auto focal_length = 1.0;
+    auto camera_center = point3(0, 0, 0);
+
+    // Calculate the vectors across the horizonral and down the vertical
+    // viewport edges
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    // Calculate the horizontal and vertical delta vectoes from pixel to pixel
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) -
+                               viewport_u / 2 - viewport_v / 2;
+    auto pixel00_loc =
+        viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     // Render
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
-    for (int i = 0; i < image_height; i++) {
-        std::clog << "\rScanlines remaining: " << (image_height - i) << ' '
+    for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' '
                   << std::flush;
-        for (int j = 0; j < image_width; j++) {
-            auto pixel_color = color(double(j) / (image_width - 1),
-                                     double(i) / (image_height - 1), 0);
+        for (int i = 0; i < image_width; i++) {
+            auto pixel_center =
+                pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
-    std::clog << "\nDone.\n";
 
+    std::clog << "\rDone.               \n";
     return 0;
 }
